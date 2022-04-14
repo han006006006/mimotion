87dc9e8a809831be3d26aa91645b1e4c9e5c4119
han006006006 <103410620+han006006006@users.noreply.github.com>
2022-04-14 09:30:01 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -1,21 +1,21 @@
 #!/bin/zsh
 
  if [[ $(date '+%H') == 08 ]];then
-     time=$(($RANDOM%10))" 08 * * *"
+     time="1 8 * * *"
  elif [[ $(date '+%H') == 10 ]];then
-     time=$(($RANDOM%10))" 10 * * *"
+     time="1 10 * * *"
  elif [[ $(date '+%H') == 11 ]];then
-     time=$(($RANDOM%10))" 11 * * *"
+     time="1 11 * * *"
  elif [[ $(date '+%H') == 12 ]];then
-     time=$(($RANDOM%10))" 12 * * *"
+     time="1 12 * * *"
  elif [[ $(date '+%H') == 13 ]];then
-     time=$(($RANDOM%10))" 13 * * *"
+     time="1 13 * * *"
  elif [[ $(date '+%H') == 17 ]];then
-     time=$(($RANDOM%5))" 17 * * *"
+     time="1 17 * * *"
  elif [[ $(date '+%H') == 19 ]];then
-     time=$(($RANDOM%10))" 19 * * *"
+     time="1 19 * * *"
  else
-     time=$(($RANDOM%10))" 8,10,11,12,13,17,19 * * *"
+     time="1 8,10,11,12,13,17,19 * * *"
  fi
 #if [[ ( $(date '+%H') == 00 ) || ( $(date '+%H') == 03 )  || ( $(date '+%H') == 05 )  || ( $(date '+%H') == 07 )  || ( $(date '+%H') == 09 )  || ( $(date '+%H') == 11 )  || ( $(date '+%H') == 13 ) ]] ;then
 #   time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
