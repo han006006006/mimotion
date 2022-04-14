f611fac63d2f56231eb115916f3972452892b810
han006006006 <103410620+han006006006@users.noreply.github.com>
2022-04-14 10:33:47 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -17,12 +17,12 @@
  #else
      time="5 8,10,11,12,13,17,19 * * *"
  #fi
-#if [[ ( $(date '+%H') == 00 ) || ( $(date '+%H') == 03 )  || ( $(date '+%H') == 05 )  || ( $(date '+%H') == 07 )  || ( $(date '+%H') == 09 )  || ( $(date '+%H') == 11 )  || ( $(date '+%H') == 13 ) ]] ;then
-#   time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
+if [[ ( $(date '+%H') == 08 ) || ( $(date '+%H') == 10 )  || ( $(date '+%H') == 11 )  || ( $(date '+%H') == 12 )  || ( $(date '+%H') == 13 )  || ( $(date '+%H') == 17 )  || ( $(date '+%H') == 19 ) ]] ;then
+   time="5 8,10,11,12,13,17,19 * * *"
     echo 当前时间为$(date '+%H')时，计划运行时间$time
     sed -i '/cron:/d' .github/workflows/run.yml
     sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
     exit 0
-#else
-#    exit 0
-#fi
+else
+    exit 0
+fi
